f1f6eaeb47b001923055586f85d3bcd6b8a1c9f3
Nadya Primak <primak.nadya@gmail.com>
2021-05-22 16:48:53 -0400

    using svg solutin instead

--- src/App.js
+++ src/App.js
@@ -4,20 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div className="App-logo" alt="logo"></div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="spinner">
+        <div class="circle">
+          <svg width="84" height="84" class="circle__svg">
+            <circle cx="41" cy="41" r="38" class="circle__progress circle__progress--path"></circle>
+            <circle cx="41" cy="41" r="38" class="circle__progress circle__progress--fill"></circle>
+          </svg>
+
+          <div class="percent">
+            <span class="percent__int">0</span>
+          </div>
+        </div>
+
+        <span class="label">Transparent</span>
+      </div>
     </div>
   );
 }
